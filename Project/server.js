df5a45c76b5540acdd0b390883063f93734eedea
rishindrareddy <rishindrareddy@hotmail.com>
2016-11-28 14:21:46 -0800

    implementing load balancer and reverse proxy

--- Project/server.js
+++ Project/server.js
@@ -7,6 +7,83 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 app.use(logger("dev"));
 
+var http = require('http');
+var loadfire = require('loadfire');
+ 
+ 
+var EDITOR_PORTS = [8081, 8082, 8083,8084];
+ 
+var EDITOR_SERVERS = EDITOR_PORTS.map(function (x) {
+    return {
+        host: 'localhost',
+        port: x
+    };
+});
+
+
+// Start all our different http servers 
+function startEditorServers(ports) {
+    ports.forEach(function (port) {
+        // Setup HTTP Server 
+     /*
+        var httpServer = http.createServer(function(req, res) {
+            // Output the port number the server is running on 
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(port.toString());
+        });
+
+        // Listen on assigned port 
+        httpServer.listen(port);
+
+        */
+
+        http.createServer(app).listen(port, function(){
+          console.log('Express server listening on port ' + port);
+          });
+ 
+        
+    });
+}
+
+
+var CONFIG = {
+    'resources': [
+        {
+            // resource is some value identify this resource 
+            // by default it should be the hostname to match 
+            resource: 'localhost:8080',
+ 
+            // List of backends to hit 
+            backends: EDITOR_SERVERS,
+ 
+            // Load balancing pattern 
+            // As of now a few are builtin 
+            // random, roundrobin, sticky 
+            pattern: 'roundrobin'
+        }
+    ],
+ 
+    // Server to start loadfire on 
+    port: 8080
+};
+
+
+function main() {
+    // Start our http servers 
+    startEditorServers(EDITOR_PORTS);
+ 
+    // Setup our load balancer with the above config 
+    var loadServer = loadfire.createServer(CONFIG);
+ 
+    // Now start our load balancer 
+    loadServer.run();
+ 
+    // Check out localhost:8080 
+    // Refresh a few times and you'll see different port numbers appear 
+    // depending on which http server the requests are proxied to 
+    // since we are using the roundrobin pattern it will cycle through them 
+}
+
 
 
 var stormpath = require('express-stormpath');
@@ -83,7 +160,8 @@ app.put('/studentlist/:id',stormpath.apiAuthenticationRequired, function (req, r
 });
 
 app.on('stormpath.ready', function() {
-  app.listen(3000);
-  console.log("Server running on port 3000");
+ // app.listen(3000);
+  //console.log("Server running on port 3000");
+  main();
 });
 
