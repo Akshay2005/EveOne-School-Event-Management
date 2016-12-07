66b79784b8b53ea94cc43cde95e95b3007068909
Akshay Sonvane <akshay.sonvane93@gmail.com>
2016-12-06 19:09:37 -0800

    Updated branch

---
+++ Project/Android/EveOne/app/src/main/java/com/akshaysonvane/eveone/utils/Utils.java
@@ -0,0 +1,10 @@
+package com.akshaysonvane.eveone.utils;
+
+/**
+ * Created by Akshay on 11/28/2016.
+ */
+
+public class Utils
+{
+    public static String BASE_URL = "https://eveone.herokuapp.com/parentLogin.html";
+}
